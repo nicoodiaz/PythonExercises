c647abc476482d2cb23382b9533f2a156263fe70
Nico Diaz <nicolas0405.nd@gmail.com>
2023-04-25 01:54:18 -0300

    Strings on python

---
+++ 04_strings/string.py
@@ -0,0 +1,19 @@
+empresa = 'Codificacion para todos'
+print(empresa)
+print(len(empresa))
+print(empresa.upper())
+print(empresa.lower())
+empresa.capitalize()
+empresa.title()
+empresa.swapcase()
+print(empresa[0:12])
+print(empresa.index('Codificacion'))
+print(empresa.replace('Codificacion', 'Python'))
+print(empresa.split(' '))
+empresa = "Facebook, Google, Microsoft, Apple, IBM, Oracle, Amazon"
+print(empresa.split(','))
+empresa = 'Codificacion para todos'
+print(empresa.index('C'))
+print(empresa.rfind('o'))
+python = ['Django', 'Flask', 'Bottle', 'Pyramid', 'Falcon']
+print('#'.join(python))
