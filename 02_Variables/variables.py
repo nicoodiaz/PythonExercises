b35437aa7a44a9bd1f16007b0c0bab627e306009
Nico Diaz <nicolas0405.nd@gmail.com>
2023-04-25 01:09:33 -0300

    Variables en python

---
+++ 02_Variables/variables.py
@@ -0,0 +1,19 @@
+name = 'Nicolas'
+last_name = 'Diaz'
+full_name = 'Nicolas Diaz'
+country = 'Argentina'
+city = 'Tucuman'
+age = 23
+year = 2023
+is_married = False
+is_true = True
+is_light_on = False
+
+name, age, year, have_pets = 'Nicolas', 23, 2023, True
+
+print(type(name))
+print(type(age))
+print(type(is_married))
+
+print(len(name))
+print(len(last_name))
