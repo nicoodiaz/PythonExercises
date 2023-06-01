db8421ee19c1d9783a9e2da091eb4042a926880c
Nico Diaz <nicolas0405.nd@gmail.com>
2023-06-01 01:04:35 -0300

    ejercicios manejo de archivos

--- 19_manejo_archivos/ej_manejo_arch.py
+++ 19_manejo_archivos/ej_manejo_arch.py
@@ -1,3 +1,5 @@
+import json
+
 def count_lines_words(rute):
     with open(rute) as f:
         lines = f.read().splitlines()
@@ -13,5 +15,25 @@ def count_lines_words(rute):
     print(count_word)
 
 
-
 count_lines_words('./files/obama_speech.txt')
+
+def more_languages(num_cnt):
+    with open('./files/countries_data.json', encoding='utf-8') as f:
+        data = json.load(f)
+    
+    idiomas = {}
+    for dictt in data:
+        idioma = tuple(dictt['languages'])
+        if idioma in idiomas:
+            idiomas[idioma] += 1
+        else:
+            idiomas[idioma] = 1
+    
+    idioma_mas_hablado = sorted(idiomas.items(), key = lambda x: x[1], reverse=True)
+
+    idiomas_top = idioma_mas_hablado[:num_cnt]
+    
+    for idioma, cantidad in idiomas_top:
+        print(f'{idioma}: {cantidad} hablantes')
+        
+more_languages(3)
